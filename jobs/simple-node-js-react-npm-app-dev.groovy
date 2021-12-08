8c92e86988d5957a6de6278e43779301087bda58
Wieslaw Pedzich <wieslaw.pedzich@gmail.com>
2021-12-08 21:13:20 +0100

    adding jobs

---
+++ jobs/simple-node-js-react-npm-app-dev.groovy
@@ -0,0 +1,31 @@
+folder("jcasc-demo") {
+}
+pipelineJob("jcasc-demo/demo-svc-dev") {
+    description("Build and deploy demo service from develop branch on development environment.")
+    keepDependencies(false)
+    disabled(false)
+    concurrentBuild(false)
+    logRotator {
+        numToKeep(500)
+        daysToKeep(30)
+    }
+    definition {
+        cpsScm {
+            scm {
+                git  {
+                    branch('*/main')
+                    remote {
+                        credentials 'git-credentials'
+                        url('https://github.com/banshee77/simple-node-js-react-npm-app.git')
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile.dev')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *') {
+            ignorePostCommitHooks(false)
+        }
+    }
+}
